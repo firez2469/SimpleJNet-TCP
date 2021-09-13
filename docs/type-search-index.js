f9aee3ebf4dbca69d0dde2b4f68e6122d8db53fe
firez2469 <47616467+firez2469@users.noreply.github.com>
2021-09-13 00:35:47 -0400

    added html back

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"Networking","l":"Client"},{"p":"Examples","l":"Network"},{"p":"Examples","l":"NetworkTesting"},{"p":"Networking","l":"Server"}]
